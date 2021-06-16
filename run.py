d074cc62f5c7d6fda8501af93eaae075c6d27bc2
JohnBiscuit <jian_hoong-99@hotmail.com>
2021-06-16 21:55:42 +0800

    Update run.py

--- run.py
+++ run.py
@@ -22,5 +22,11 @@ while (1):
         a_z = (acc_z / (2.0 ** 15.0)) * accel_sens  #convert to G's of acceleration
         a_x = (acc_x / (2.0 ** 15.0)) * accel_sens
         acc_roll = math.degrees(math.atan(a_x/a_z))
+        acc_y = read_raw_bits(ACCEL_YOUT_H)
+        acc_z = read_raw_bits(ACCEL_ZOUT_H)
+        a_y = (acc_y / (2.0 ** 15.0)) * accel_sens
+        a_z = (acc_z / (2.0 ** 15.0)) * accel_sens
+        acc_pitch = math.degrees(math.atan(a_y/a_z))
     except:
         continue
+        print(acc_roll)
