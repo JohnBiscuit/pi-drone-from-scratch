e6881b59410e4f8182b8186dfcd5d7755fd974b3
JohnBiscuit <jian_hoong-99@hotmail.com>
2021-06-16 16:13:30 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,12 +2,16 @@ from mpu9250 import *
 import scipy.integrate as integrate
 #import matplotlib.pyplot as plt
 import time
+import smbus
 
 alpha = 0.98
 t_prev = time.time()
 gyro_x_prev = 0
 gyro_z_prev = 0
 gyro_y_prev = 0
+
+time.sleep(1)
+
 while (1):
     t_now = time.time()
     elapsed = t_now-t_prev
