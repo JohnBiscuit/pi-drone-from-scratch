404588972a80db224d50111b22de787f626fc6be
JohnBiscuit <jian_hoong-99@hotmail.com>
2021-06-16 15:58:27 +0800

    Update run.py

--- run.py
+++ run.py
@@ -19,15 +19,18 @@ while (1):
         gyro_z = gyro_z(elapsed) #roll
     except:
         continue
+    try:
+        gyro_pitch = gyro_pitch(gyro_x_prev,gyro_x,t_prev)
+        gyro_roll = gyro_roll(gyro_y_prev,gyro_y,t_prev)
+        gyro_yaw = gyro_yaw(gyro_z_prev,gyro_z,t_prev)
 
-    gyro_pitch = gyro_pitch(gyro_x_prev,gyro_x,t_prev)
-    gyro_roll = gyro_roll(gyro_y_prev,gyro_y,t_prev)
-    gyro_yaw = gyro_yaw(gyro_z_prev,gyro_z,t_prev)
-
-    pitch = (gyro_x*alpha)+(acc_pitch*(1-alpha))
-    roll = (gyro_y*alpha)+(acc_roll*(1-alpha))
-    gyro_x_prev = gyro_pitch
-    gyro_y_prev = gyro_roll
-    gyro_z_prev = gyro_yaw
-    t_prev =t_now
-    print(pitch)
+        pitch = (gyro_x*alpha)+(acc_pitch*(1-alpha))
+        roll = (gyro_y*alpha)+(acc_roll*(1-alpha))
+        gyro_x_prev = gyro_pitch
+        gyro_y_prev = gyro_roll
+        gyro_z_prev = gyro_yaw
+        t_prev =t_now
+        print(pitch)
+    except:
+        print("error")
+        continue
