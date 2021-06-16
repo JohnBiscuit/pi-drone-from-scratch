7c6c6fc8b0f999d4defe13189ecb2a105cdd5721
JohnBiscuit <jian_hoong-99@hotmail.com>
2021-06-16 21:59:03 +0800

    Update run.py

--- run.py
+++ run.py
@@ -16,17 +16,7 @@ while (1):
     t_now = time.time()
     elapsed = t_now-t_prev
     try:
-        ax,ay,az,wx,wy,wz= mpu6050_conv()
-        acc_x = ax        #get accelerometer reading of x axis
-        acc_z = az        #get accelerometer reading of x axis
-        a_z = (acc_z / (2.0 ** 15.0)) * accel_sens  #convert to G's of acceleration
-        a_x = (acc_x / (2.0 ** 15.0)) * accel_sens
-        acc_roll = math.degrees(math.atan(a_x/a_z))
-        acc_y = read_raw_bits(ACCEL_YOUT_H)
-        acc_z = read_raw_bits(ACCEL_ZOUT_H)
-        a_y = (acc_y / (2.0 ** 15.0)) * accel_sens
-        a_z = (acc_z / (2.0 ** 15.0)) * accel_sens
-        acc_pitch = math.degrees(math.atan(a_y/a_z))
+        a_x, a_y, a_z, w_x, w_y, w_z = mpu6050_conv()
     except:
         continue
-        print(acc_roll)
+        print(a_x)
