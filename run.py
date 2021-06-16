0760d873cba6ee4f9cb9a22277c6eee9f30bd5bd
JohnBiscuit <jian_hoong-99@hotmail.com>
2021-06-17 00:28:29 +0800

    Update run.py

--- run.py
+++ run.py
@@ -35,7 +35,7 @@ while (1):
         gyro_x_current = gyro_x(wx,elapsed)
         gyro_y_current = gyro_y(wy,elapsed)
         gyro_z_current = gyro_z(gyro_z,elapsed)
-        alpha = elapsed = (1/(1+elapsed))
+        #alpha = elapsed = (1/(1+elapsed))
         #gyro pitch
         dt[1] = elapsed
         data[0] = gyro_x_prev
@@ -47,7 +47,7 @@ while (1):
         data[0] = gyro_y_prev
         data[1] = gyro_y_current
         area = integrate.trapz(dt,data)
-        gyro_roll = roll_angle +area
+        gyro_roll = roll_angle + area
         #gyro yaw
         dt[1] = elapsed
         data[0] = gyro_y_prev
