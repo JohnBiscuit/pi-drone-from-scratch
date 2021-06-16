91ff6535f71e881d33350c1036d84c3abdebd5fa
JohnBiscuit <jian_hoong-99@hotmail.com>
2021-06-16 23:06:45 +0800

    Update run.py

--- run.py
+++ run.py
@@ -30,8 +30,10 @@ while (1):
         gyro_x_current = gyro_x(wx,elapsed)
         gyro_y_current = gyro_y(wy,elapsed)
         gyro_z_current = gyro_z(gyro_z,elapsed)
-        
+        gyro_pitch = gyro_pitch(gyro_x_prev,gyro_x,current,elapsed)
     except:
         continue
+    t_prev = t_now
+    gyro_x_prev = gyro_x_current
 
     print(gyro_z_current)
