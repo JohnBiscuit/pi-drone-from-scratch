bbf15c79d6e81b11d52c23274c30994edd020dd9
JohnBiscuit <jian_hoong-99@hotmail.com>
2021-06-17 09:54:39 +0800

    Update run.py

--- run.py
+++ run.py
@@ -63,5 +63,5 @@ while (1):
     gyro_x_prev = gyro_x_current
     gyro_y_prev = gyro_y_current
     gyro_z_prev = gyro_z_current
-    print(acc_roll)
-    #print("roll:",gyro_roll.round(2),"pitch:",gyro_pitch.round(2),"yaw:",gyro_yaw.round(0),"roll angle:",roll_angle.round(2))
+    #print(acc_roll)
+    print("roll:",gyro_roll.round(2),"pitch:",gyro_pitch.round(2),"yaw:",gyro_yaw.round(0),"roll angle:",roll_angle.round(2))
