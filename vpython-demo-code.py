1b7fdc01332a4a89b9db8410dfc9ef4213285ffe
johnbiscuit <jian_hoong-99@hotmail.com>
2021-06-19 14:28:20 +0800

    __pycache__/ 	run.py.save

--- vpython-demo-code.py
+++ vpython-demo-code.py
@@ -1,3 +1,6 @@
 from vpython import *
+import time
 x = box(length=6,width= 3,height = 0.1)
-x.rotate(angle = -45)
+while(1):
+    time.sleep(0.001)
+    x.rotate(angle = -0.001,axis =vector(0,1,0))
