9fffd0aa1df4261ae392f6246d21ffbf847588f2
johnbiscuit <jian_hoong-99@hotmail.com>
2021-06-26 13:05:23 +0800

    modified:   vpython-demo-code.py

--- vpython-demo-code.py
+++ vpython-demo-code.py
@@ -1,6 +1,13 @@
 from vpython import *
 import time
-x = box(length=6,width= 3,height = 0.1)
+x = box(length=6,width= 3,height = 0.1,opacity=1,shininess=0.1,color=color.magenta)
+pointer = arrow(pos=vector(0,0,1),axis=vector(2,0,0), shaftwidth=0.1)
+pointer2 = arrow(pos=vector(0,0,1),axis=vector(0,2,0), shaftwidth=0.1,color = color.red)
+pointer3 = arrow(pos=vector(0,0,1),axis=vector(0,0,2), shaftwidth=0.1)
+
+
 while(1):
-    time.sleep(0.001)
+    time.sleep(0.0015)
     x.rotate(angle = -0.001,axis =vector(0,1,0))
+    x.rotate(angle = -0.001,axis =vector(1,0,0))
+    x.rotate(angle = -0.001,axis =vector(0,0,1))
